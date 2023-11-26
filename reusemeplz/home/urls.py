92d01b02e20e4ed3f4b1b022de3ec50acb19ef1f
johnyssho <hon.brod@email.cz>
2023-11-26 21:02:48 +0100

    next bit of rewrite

--- reusemeplz/home/urls.py
+++ reusemeplz/home/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import HomeView, CloudNavodView, BootTipsView
+from .views import HomeView, CloudNavodView, BootTipsView, DebianInstalaceView, AptView
 
 urlpatterns = [
     path('', HomeView, name='home'),
     path('cloud-navod/', CloudNavodView, name='cloudnávod'),
     path('boot-tips/', BootTipsView, name='boot-tips'),
+    path('debian-instalace/', DebianInstalaceView, name='debian-instalace'),
+    path('apt/', AptView, name='apt'),
 ]
--- reusemeplz/home/views.py
+++ reusemeplz/home/views.py
@@ -8,3 +8,9 @@ def CloudNavodView(request):
 
 def BootTipsView(request):
   return render(request, 'boot-tips.html')
+
+def DebianInstalaceView(request):
+  return render(request, 'debian-instalace.html')
+
+def AptView(request):
+  return render(request, 'apt.html')
