866a21bb26890151ceb6a2cdfc8ee5451364b38f
johnyssho <hon.brod@email.cz>
2024-02-29 12:57:07 +0100

    prod readying

---
+++ bake.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rsync -ravuP --delete reusemeplz/reusemeplz/* prod/reusemeplz/
+
+cat secret/key > prod/reusemeplz/reusemeplz/secret_key.txt
+
+rm prod/reusemeplz/reusemeplz/settings.py
+
+cp settings.py prod/reusemeplz/reusemeplz/settings.py
+
